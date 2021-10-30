4c4912cf65c0aa8b42f3312e06ba7b7ab3cf9037
Aleksandr.Bakiyev <Aleksandr.Bakiyev@nexign.com>
2021-10-31 01:03:10 +0300

    - Added react-query && zustand

--- src/main/webapp/app/index.tsx
+++ src/main/webapp/app/index.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import { QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
-import getStore from './config/store';
-import setupAxiosInterceptors from './config/axios-interceptor';
-import { clearAuthentication } from './shared/reducers/authentication';
 import ErrorBoundary from './shared/error/error-boundary';
 import AppComponent from './app';
 import { loadIcons } from './config/icon-loader';
-
-const store = getStore();
-
-const actions = bindActionCreators({ clearAuthentication }, store.dispatch);
-setupAxiosInterceptors(() => actions.clearAuthentication('login.error.unauthorized'));
+import { queryClient } from './config/query-client';
 
 loadIcons();
 
@@ -23,11 +16,12 @@ const render = Component =>
   // eslint-disable-next-line react/no-render-return-value
   ReactDOM.render(
     <ErrorBoundary>
-      <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
         <div>
           <Component />
         </div>
-      </Provider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </ErrorBoundary>,
     rootEl
   );
