bc50b39f34f69ab4dc94b70a22a918f68a4f0e70
Aleksandr.Bakiyev <Aleksandr.Bakiyev@nexign.com>
2021-11-05 03:05:52 +0300

    - Added CacheKeyType enumeration

---
+++ src/main/webapp/app/enums/CacheKeyType.ts
@@ -0,0 +1,11 @@
+enum CacheKeyType {
+  ANIME = 'anime',
+  ANIME_CHARACTERS = 'anime-characters',
+  ANIME_PICTURES = 'anime-pictures',
+  SEARCH_ANIME = 'search_anime',
+  SEARCH_MANGA = 'search_manga',
+  TOP_ANIME = 'top_anime',
+  TOP_MANGA = 'top_manga',
+}
+
+export default CacheKeyType;
